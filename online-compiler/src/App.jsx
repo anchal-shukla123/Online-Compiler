0306b34fa58ba205573da250adb37113f09a1daa
anchal-shukla123 <anchalshukla423@gmail.com>
2026-01-28 21:21:51 +0530

    Added footer with privacy policy

--- online-compiler/src/App.jsx
+++ online-compiler/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, UNSAFE_WithComponentProps } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Compiler from './pages/Compiler';
 import About from './pages/About';
-
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Footer from './components/Footer';
 /**
  * Main App Component
  * Sets up routing and renders the application
@@ -18,7 +19,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/compiler" element={<Compiler />} />
           <Route path="/about" element={<About />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
